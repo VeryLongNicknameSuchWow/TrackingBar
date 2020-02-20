addda02007acac8c33f0fa0a9aedcd586b9c7044
verylongnicknamesuchwow <d33p4ck@gmail.com>
2020-02-20 12:55:57 +0100

    initial commit

---
+++ src/main/java/pl/rynbou/TrackingBar/TrackingBarMain.java
@@ -0,0 +1,16 @@
+package pl.rynbou.TrackingBar;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TrackingBarMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
