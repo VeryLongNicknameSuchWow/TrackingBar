7498cebd4b69df0498259f79c6e9eb7f2aaeb57b
verylongnicknamesuchwow <d33p4ck@gmail.com>
2020-02-20 21:18:15 +0100

    deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-BUILD_NUMBER = ls | grep '^[0-9]\+$' | cut -c2- | sort -n | tail -n1 | sed 's|^|r|'
+BUILD_NUMBER=$(ls | grep '^[0-9]\+$' | cut -c2- | sort -n | tail -n1 | sed 's|^|r|')
 rm /home/minecraft/servers/1.12.2/plugins/trackingbar-*.jar
 cp /var/lib/jenkins/jobs/TrackingBar/builds/$BUILD_NUMBER/archive/target/*.jar /home/minecraft/servers/1.12.2/plugins/
