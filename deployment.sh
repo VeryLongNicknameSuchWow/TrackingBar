ffe3ed910dcd4a5a0cfb9efb9516f98620eb22e2
verylongnicknamesuchwow <d33p4ck@gmail.com>
2020-02-21 21:12:28 +0100

    remove deployment.sh, change artifactId

--- deployment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-BUILD_NUMBER=$(exec ls /var/lib/jenkins/jobs/TrackingBar/builds/ | sed 's/\([0-9]\+\).*/\1/g' | sort -n | tail -1)
-rm /home/minecraft/servers/1.12.2/plugins/trackingbar-*.jar
-cp /var/lib/jenkins/jobs/TrackingBar/builds/$BUILD_NUMBER/archive/target/*.jar /home/minecraft/servers/1.12.2/plugins/
