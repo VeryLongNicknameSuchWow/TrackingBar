5f1b6b8e5173b3ed3ad36d30353db1f5c472cefe
BuildTools <szymonwitek02@gmail.com>
2020-02-22 23:11:18 +0100

    Add ItemStack loader

---
+++ src/main/java/pl/rynbou/trackingbar/util/ItemUtil.java
@@ -0,0 +1,51 @@
+package pl.rynbou.trackingbar.util;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemUtil {
+
+    private ItemStack loadItemStack(ConfigurationSection itemSection) {
+        String itemString = itemSection.getString("type");
+        if (itemString == null) {
+            return null;
+        }
+        ItemStack itemStack;
+        Material material = Material.getMaterial(itemString);
+        if (material == null) {
+            return null;
+        }
+        itemStack = new ItemStack(material);
+
+        String name = itemSection.getString("name");
+        if (name != null && !name.isEmpty()) {
+            String nameColor = StrUtil.color(name);
+            ItemMeta im = itemStack.getItemMeta();
+            if (im != null) {
+                im.setDisplayName(nameColor);
+                itemStack.setItemMeta(im);
+            }
+        }
+
+        List<String> lore = itemSection.getStringList("lore");
+        if (!lore.isEmpty()) {
+            List<String> loreColor = new ArrayList<>();
+            lore.forEach(s -> loreColor.add(StrUtil.color(s)));
+            ItemMeta im = itemStack.getItemMeta();
+            if (im != null) {
+                im.setLore(loreColor);
+                itemStack.setItemMeta(im);
+            }
+        }
+
+        //todo enchants
+
+        return itemStack;
+    }
+
+}
