e0695a449defe1737dd97a48720af5b3c492d498
verylongnicknamesuchwow <d33p4ck@gmail.com>
2020-02-26 15:19:48 +0100

    add enchant support

--- src/main/java/pl/rynbou/trackingbar/util/ItemUtil.java
+++ src/main/java/pl/rynbou/trackingbar/util/ItemUtil.java
@@ -1,7 +1,9 @@
 package pl.rynbou.trackingbar.util;
 
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -44,21 +46,20 @@ public class ItemUtil {
         }
 
         //TODO enchants
-//        List<String> enchants = itemSection.getStringList("enchants");
-//        if (!enchants.isEmpty()) {
-//            ItemMeta im = itemStack.getItemMeta();
-//            if (im != null) {
-//                for (String s : enchants) {
-//                    String enchant = s.split(":")[0];
-//                    int power = Integer.parseInt(s.split(":")[1]);
-//                    Enchantment enchantment = Enchantment.getByKey(NamespacedKey.minecraft(enchant));
-//                    if (enchantment == null)
-//                        continue;
-//                    im.addEnchant(enchantment, power, true);
-//                }
-//            }
-//
-//        }
+        List<String> enchants = itemSection.getStringList("enchants");
+        if (!enchants.isEmpty()) {
+            ItemMeta im = itemStack.getItemMeta();
+            if (im != null) {
+                for (String s : enchants) {
+                    String enchant = s.split(":")[0];
+                    int power = Integer.parseInt(s.split(":")[1]);
+                    Enchantment enchantment = Enchantment.getByKey(NamespacedKey.minecraft(enchant));
+                    if (enchantment == null)
+                        continue;
+                    im.addEnchant(enchantment, power, true);
+                }
+            }
+        }
 
         return itemStack;
     }
