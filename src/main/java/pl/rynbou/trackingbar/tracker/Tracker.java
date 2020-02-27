14d255efefbaef4fcb83afd27636e44c1c3e2c5a
verylongnicknamesuchwow <d33p4ck@gmail.com>
2020-02-27 22:32:10 +0100

    remove user from tracked.TrackedBy on tracker disable

--- src/main/java/pl/rynbou/trackingbar/tracker/Tracker.java
+++ src/main/java/pl/rynbou/trackingbar/tracker/Tracker.java
@@ -160,6 +160,7 @@ public class Tracker {
     public void disable(Player player) {
         User user = getUser(player);
         plugin.getMessages().sendToggleOffMessage(player);
+        getUser(user.getTracking()).getTrackedBy().remove(player);
         user.setTracking(null);
         clearBossBar(player);
     }
