9c7939b118bf87a70479bb74b4318a6c851fc3d4
verylongnicknamesuchwow <d33p4ck@gmail.com>
2020-02-27 19:14:22 +0100

    commands base

--- src/main/java/pl/rynbou/trackingbar/cmds/TrackerCommand.java
+++ src/main/java/pl/rynbou/trackingbar/cmds/TrackerCommand.java
@@ -25,13 +25,57 @@ public class TrackerCommand implements CommandExecutor {
         Player player = (Player) sender;
         player.getInventory().addItem(plugin.getSettings().getTrackerItem());
 
-        if (args.length != 1) {
-            sender.sendMessage("Correct usage: /tracker <name>");
+        if (args.length == 0) {
+            sendHelp(player);
             return true;
         }
 
+        if (args.length == 1) {
+            switch (args[0]) {
+                case "next":
+                    plugin.getTracker().cycle(player);
+                    return true;
+                case "nearest":
+                    plugin.getTracker().trackClosest(player);
+                    return true;
+                case "disable":
+                    plugin.getTracker().disable(player);
+                    return true;
+                case "friend":
+                    player.sendMessage("Correct usage: /tracker friend <nickname>");
+                    return true;
+                default:
+                    player.sendMessage("Incorrect argument \"" + args[0] + "\"");
+                    sendHelp(player);
+                    return true;
+            }
+        }
+
+        if (args.length == 2) {
+            switch (args[0]) {
+                case "friend":
+                    Player friend = Bukkit.getPlayer(args[1]);
+                    if (friend == null) {
+                        player.sendMessage("That player is not online!");
+                        return true;
+                    }
+                    plugin.getTracker().toggleFriend(player, friend);
+                    return true;
+                default:
+                    sendHelp(player);
+                    return true;
+            }
+        }
 
         plugin.getTracker().track(player, Bukkit.getPlayer(args[0]));
         return true;
+    }
+
+    public void sendHelp(Player player) {
+        player.sendMessage("Correct usage:");
+        player.sendMessage("/tracker next    - Start tracking next player");
+        player.sendMessage("/tracker nearest - Start tracking the nearest player");
+        player.sendMessage("/tracker disable - Disable tracker");
+        player.sendMessage("/tracker friend  - Toggle friendship status");
     }
 }
